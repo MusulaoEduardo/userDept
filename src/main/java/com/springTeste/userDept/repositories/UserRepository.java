9374a4d9adf573ed7fd27aee35c27ef970d14f12
EduardoMusulao <eduardomusulao878@gmail.com>
2024-05-13 20:52:38 -0300

    Revert "teste do spring boot"
    This reverts commit d3fb3a8026b85dc635b7753cf675b02560e309f9.

--- src/main/java/com/springTeste/userDept/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springTeste.userDept.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springTeste.userDept.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO TB_DEPARTAMENT(name) VALUES ('Gestão');
-INSERT INTO TB_DEPARTAMENT(name) VALUES ('Informática');
-
-INSERT INTO TB_USER(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
-INSERT INTO TB_USER(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
-INSERT INTO TB_USER(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
-INSERT INTO TB_USER(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
